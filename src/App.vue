efe84664663781e86e26e6b1a74bd730053e66b8
hvekriya <haresh.vekriya@gmail.com>
2021-08-06 12:58:33 +0100

    Initial quiz

--- src/App.vue
+++ src/App.vue
@@ -9,21 +9,6 @@
 </template>
 
 <style lang="scss">
-/* Import Vue Flow Form base CSS */
-@import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.css";
-/* Import one of the Vue Flow Form CSS themes (optional) */
-@import "~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-minimal.css";
-/* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-green.css'; */
-/* @import '~@ditdot-dev/vue-flow-form/dist/vue-flow-form.theme-purple.css'; */
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
 #nav {
   padding: 30px;
 
--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+window.$ = window.jQuery = require('jquery');
+
 Vue.config.productionTip = false
 
 new Vue({
